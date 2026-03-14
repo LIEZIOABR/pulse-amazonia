37083f3d6bb1e76e1ecc2863218b3f452ba0eddb
LIEZIOABR <150058055+LIEZIOABR@users.noreply.github.com>
2026-03-14 20:05:07 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,18 @@
-const CACHE_NAME = 'pulse-amazonia-v1';
-const urlsToCache = [
-  '/radar-amazonia.html',
-  '/manifest.json',
-  '/img/logo-abr-allinone.png'
-];
+const CACHE_NAME = 'pulse-amazonia-v2';
 
 self.addEventListener('install', event => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => caches.delete(key)))
+    )
   );
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+  event.respondWith(fetch(event.request));
+});
 });
